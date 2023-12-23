41d9b27f96e70da90c05e0d7d49ea02af9728cbe
Cnou9 <nm942002@GMAIL.COM>
2023-12-23 14:42:18 +0700

    .

--- Lab11/src/main/java/com/example/demo1/database/InitData.java
+++ Lab11/src/main/java/com/example/demo1/database/InitData.java
@@ -2,6 +2,7 @@ package com.example.demo1.database;
 
 import com.example.demo1.database.BookDB;
 import com.example.demo1.utils.CSVFileReader;
+import com.example.demo1.utils.ExcelFileReader;
 import com.example.demo1.utils.JsonFileReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,14 +11,23 @@ import org.springframework.context.annotation.Configuration;
 // CommandLineRunner là một interface chạy 1 lần sau khi Spring Boot khởi động
 @Configuration
 public class InitData implements CommandLineRunner {
+//    @Autowired
+//    private JsonFileReader jsonFileReader;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        BookDB.bookList = jsonFileReader.readFile("MOCK_DATA.json");
+//        System.out.println("Book size : " + BookDB.bookList.size());
+//    }
     @Autowired
-    private JsonFileReader jsonFileReader;
+    private ExcelFileReader excelFileReader;
 
     @Override
     public void run(String... args) throws Exception {
-        BookDB.bookList = jsonFileReader.readFile("MOCK_DATA.json");
+        BookDB.bookList = excelFileReader.readFile("classpath:static/data.xlsx");
         System.out.println("Book size : " + BookDB.bookList.size());
     }
+
 
 //    @Autowired
 //    private CSVFileReader csvFileReader;
